7ce875b25de23ff4b4ae597cfba68f5dc43ae879
Anthony Botticchio <anthonyjb5228@gmail.com>
2026-01-11 19:01:12 -0500

    Some changes

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -3,12 +3,11 @@ set -e
 
 echo "Installing extra software..."
 
-sudo apt update >/dev/null
+sudo apt update
 sudo apt install -y \
     ros-humble-navigation2 \
     ros-humble-nav2-bringup \
-    ros-humble-ros-ign-bridge \
-    ros-humble-ros-gzharmonic >/dev/null
+    ros-humble-ros-gzharmonic
 
 echo "Starting Micro XRCE Agent..."
 MicroXRCEAgent udp4 -p 8888
