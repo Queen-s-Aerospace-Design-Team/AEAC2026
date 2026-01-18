ea0ed0223b882e8765cebf3d8fafd590e85c00e0
Anthony Botticchio <anthonyjb5228@gmail.com>
2026-01-18 17:58:35 -0500

    Chanegs

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -12,13 +12,3 @@ sudo apt install -y \
 
 echo "Starting Micro XRCE Agent..."
 MicroXRCEAgent udp4 -p 8888
-
-# Lidar 2D:
-# ros2 run ros_gz_bridge parameter_bridge /lidar@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked
-# Frame: x500_lidar_2d_0/link/lidar_2d_v2 # We can always map it to 'map'
-
-# Depth:
-# ros2 run ros_gz_bridge parameter_bridge /depth_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked
-# Frame: x500_depth_0/camera_link/StereoOV7251
-
-# Make a tmux script that launches these automatically
