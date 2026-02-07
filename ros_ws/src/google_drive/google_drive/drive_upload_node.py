885352c804fe67d2fb66fd7e9f418cae62389704
Siera Beckett <siera.beckett@icloud.com>
2026-02-07 14:35:14 -0500

    second upload for google drive node - Siera's work

---
+++ ros_ws/src/google_drive/google_drive/drive_upload_node.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+#from px4_msgs.msg import
+
+
+
+
+#configure Qos for publishing and subscribing
+
+#create publishers
+
+#create subscribers 
+
+
+#initialize variables
+
+self.timer_period = 1.0; 
+self.
+
+#create timer to publish control commands 
+
+
+#define functions
+
+
+
+#upload media file to google drive 
+
+# node = drive_upload_service
+
+#scp media_file, ges "secure copy"
+
+#ssh ges python3 uploadMedia.py
+
+# -/drive.upload --> std_msgs/String 
+
+
+
+def main(args=None):
+    
