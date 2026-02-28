4a55ddfbff26c71b8f8c2d8c83bf193c328ea976
Anthony Botticchio <anthonyjb5228@gmail.com>
2026-02-28 14:42:21 -0500

    Upload drive formatting edits. Removed drive/ folder

--- ros_ws/src/google_drive/google_drive/drive_upload_node_SB.py
+++ ros_ws/src/google_drive/google_drive/drive_upload_node_SB.py
@@ -1,86 +1,60 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
- 
-import sys
-import os
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 from upload_photo import upload_image
  
-from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
- 
- 
-#configure Qos for publishing and subscribing
 class drive_upload_service(Node):
     def __init__(self):
         super().__init__('drive_upload_service')
  
-    qos_profile = QoSProfile(
-        reliability=ReliabilityPolicy.RELIABLE,
-        durability=DurabilityPolicy.TRANSIENT_LOCAL,
-        history=HistoryPolicy.KEEP_LAST,
-        depth=1,
-    )
-   
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1,
+        )
+        
+        # I don't believe we need any publishers for this node
+        # create publishers
+        # self.offboard_control_mode_publisher = self.create_publisher(
+                    #OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
+        
+        # create subscribers
+        # I am not sure which part of this is correct, but I think they both work, they are just two different versions.
+        self.drive_upload_subscriber = self.create_subscription(
+            String,                 # Message type --> std_msgs/msg/String
+            '/drive_upload',        # Topic name
+            self.drive_upload_callback, # Callback function
+            10                      # Queue size (QoS depth)
+        )
  
-# I don't believe we need any publishers for this node
-#create publishers
-#self.offboard_control_mode_publisher = self.create_publisher(
-            #OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
+    # Upload media file to google drive
+    def drive_upload_callback(self, msg): 
+        self.get_logger().info(f"I heard: {msg.data}")
+    
+        # this is the part I'm really not sure about, and how it works
+        # this is supposed to be the part that triggers the upload_image() funciton in upload_photo.py
+    
+        # I thought I was supposed to use the commands:
+        # scp media_file, ges "secure copy"
+        # ssh ges python3 uploadMedia.py
+    
+        file_path = msg.data
+    
+        self.get_logger().info(f"Received: {file_path}")
+    
+        try:
+            upload_image()
+            self.get_logger().info("Upload successful")
+        except Exception as e:
+            self.get_logger().error(f"Upload failed: {e}")
  
- 
- 
- 
-#create subscribers
-#I am not sure which part of this is correct, but I think they both work, they are just two different versions.
-self.drive_upload_subscriber = self.create_subscription(
-    String,                 # Message type --> std_msgs/msg/String
-    '/drive_upload',        # Topic name
-    self.drive_upload_callback, # Callback function
-    10                      # Queue size (QoS depth)
-)
- 
-#self.drive_upload_subscriber = self.create_subscription(
-    #String, '/drive.upload', self.drive_upload_callback, qos_profile)
- 
- 
- 
-#initialize variables
- 
- 
-#define functions
-def drive_upload_callback(self, msg):
-    #upload media file to google drive
-    self.get_logger().info(f"I heard: {msg.data}")
- 
- 
-    # this is the part I'm really not sure about, and how it works
-    #this is supposed to be the part that triggers the upload_image() funciton in upload_photo.py
-   
-    # I thought I was supposed to use the commands:
-    #scp media_file, ges "secure copy"
-    #ssh ges python3 uploadMedia.py
- 
-    file_path = msg.data
- 
-    self.get_logger().info(f"Received: {file_path}")
- 
-    try:
-        upload_image()
-        self.get_logger().info("Upload successful")
- 
-    except Exception as e:
-        self.get_logger().error(f"Upload failed: {e}")
- 
- 
- 
- 
-#main loop
+
 def main(args=None):
     rclpy.init(args=args)
- 
-    node = MySubscriber()
+    node = drive_upload_service()
     rclpy.spin(node)
- 
     node.destroy_node()
     rclpy.shutdown()
  
